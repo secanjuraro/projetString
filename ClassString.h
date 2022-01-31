89ddd0eaf7f46d7200be3bdfbf72ffb41f20f076
MaevaBeugin <94899228+MaevaBeugin@users.noreply.github.com>
2022-01-31 14:18:53 +0100

    Merged with student A functions

--- ClassString.h
+++ ClassString.h
@@ -7,26 +7,30 @@ class ClassString
 {
   public:
   	ClassString(); //default constructor
+	  ClassString(const ClassString& str);//copy constructor
     ClassString(const char*); //constructor from a c-string
     ~ ClassString(); //destructor
     void printmot();
+	  const char* c_str();
+	  int size();
     int length() const;
     int max_size() const;
-    ClassString& operator=(const ClassString&);
     ClassString& resize(std::size_t, char);
     int capacity();
+	  void clear();
     bool empty();
     void reserve(std::size_t);
     ClassString& operator=(const char*);
+	  ClassString& operator=(char c);
+    ClassString& operator=(const ClassString&);
+    friend ClassString operator+(const ClassString& lhs,const char* rhs);
     friend ClassString operator+(const ClassString&, const ClassString&);
+    friend ClassString operator+(const ClassString&, char);
 
   protected:
   	char* p;
     int sizeString;
     int memReserved;
 };
-
-//ClassString operator+(const ClassString&, char);
-
 
 #endif
