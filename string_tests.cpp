32d04a6b9a907ebbb98389cf144c1f0f039667c7
MaevaBeugin <94899228+MaevaBeugin@users.noreply.github.com>
2022-01-31 14:19:38 +0100

    Merged with student A functions

--- string_tests.cpp
+++ string_tests.cpp
@@ -23,9 +23,26 @@ int main(){
     chain[11]= '\0';
 
     ClassString* mot1 = new ClassString(chain);
-    mot1 -> printmot();
+	
+	//Test functions Student A (Blanc Théo)
+    std::cout << "****  Student A :  ****" << std::endl;
+	ClassString* mot4 = mot1; // test of the copy constructor
+	mot4 -> printmot();
+	const char* j = mot4->c_str();
+	ClassString* mot5 = new ClassString(j); // test of the c_str function
+	mot5->printmot();
+	std::cout <<"Word size is : " << mot4 -> size()<< std::endl;
+	mot5->clear();
+	mot5->printmot();
+    *mot5 = 'a'; // testing the = operator
+	mot5->printmot();
+    ClassString mot6 = *mot5 + "fff";
+    mot6.printmot();
+	
+    std::cout << std::endl;
 
     // Test functions Student B (Maëva Beugin)
+    std::cout << "****  Student B :  ****" << std::endl;
     std::cout <<"Word length is : " << mot1 -> length()<< std::endl;
     std::cout <<"The maximum size of a word is : " << mot1 -> max_size()<< std::endl;
 
@@ -39,11 +56,12 @@ int main(){
     mot2 -> printmot();
     ClassString* motOperatorEgal = mot2;
     std::cout <<"Test operator=(const ClassString&) : "<< std::endl; motOperatorEgal -> printmot();
-    //ClassString* motOperatorPlus = mot1 + charEssay;
-    //std::cout <<"Test operator+(const ClassString&, char) : "<< std::endl;
-    //motOperatorPlus->printmot();
+    std::cout <<"Test operator+(const ClassString&, char) : "<< std::endl;
+    ClassString mot7 = *mot5 + 'd';
+    mot7.printmot();
 
     std::cout << std::endl;
+
     // Test functions Student C (Sonia Canjura)
     char* chain3 = new char[100];
     memcpy(chain3, chain, chain_mem);
@@ -54,8 +72,11 @@ int main(){
     std::cout << "reserve(120): old memReserved = " << mot3.capacity(); mot3.reserve(120); std::cout << ", new memReserved = " << mot3.capacity() << std::endl;
     std::cout << "reserve(12): old memReserved = " << mot3.capacity(); mot3.reserve(12); std::cout << ",  new memReserved = " << mot3.capacity() << std::endl;
     std::cout <<"operator+(mot3, mot3): "; (mot3 + mot3).printmot();
+
+    std::cout << std::endl;
     /* Manual deletion of pointers */
     delete mot1; //mot1 is not deleted automatically
     delete mot2; //mot2 is not deleted automatically
+	delete mot5;
     delete[] chain3; //chain3 is not deleted automatically
  }
